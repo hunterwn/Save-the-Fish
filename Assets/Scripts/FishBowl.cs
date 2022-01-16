23c99f5f60fce9a38111984aba51371e6191e5b1
hunterwn <hunterwnorman@gmail.com>
2022-01-16 01:14:03 -0500

    Added water meter

---
+++ Assets/Scripts/FishBowl.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class FishBowl : MonoBehaviour
+{
+    public int waterLayer = 6;
+    public GameObject meter;
+    private int waterCount = 0;
+
+    private Vector3 change = new Vector3( 0, 0.001f, 0); 
+    
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if(collider.gameObject.layer == waterLayer)
+        {
+            waterCount += 1;
+            meter.transform.localScale += change;
+            meter.transform.position += change;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider) {
+        if(collider.gameObject.layer == waterLayer)
+        {
+            waterCount -= 1;
+            meter.transform.localScale -= change;
+            meter.transform.position -= change;
+        }
+    }
+}
