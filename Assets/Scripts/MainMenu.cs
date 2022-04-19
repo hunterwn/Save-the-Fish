95edea79edfabf23b8f3289d98a6007b95599451
jrymkos <Jrsmash@yahoo.com>
2022-04-18 20:15:36 -0400

    Added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{   
+    public string level1;
+    public string level2;
+    public string level3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(level1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
