409f0a1caa44ed4aedbbd8359eac88bf2370f114
Tri-Wing <hunterwnorman@gmail.com>
2022-04-22 18:02:18 -0400

    added fan sound

--- Assets/Scripts/fan.cs
+++ Assets/Scripts/fan.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class fan : MonoBehaviour
 {
+    bool waiting = false;
+    public AudioSource fanSound;
     public Vector2 addedForce = new Vector2(1, 0);
+    public Animator animator;
+    private void Start() {
+        animator.enabled = false;
+    }
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.CompareTag("Particle")) {
+            if(!waiting) {
+                fanSound.Play();
+                StartCoroutine(WaitCoroutine());
+            }
             Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
             rb.AddForce(addedForce);
         }
@@ -16,5 +26,14 @@ public class fan : MonoBehaviour
             Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
             rb.AddForce(addedForce);
         }
+    }
+
+    IEnumerator WaitCoroutine()
+    {
+        waiting = true;
+        animator.enabled = true;
+        yield return new WaitForSeconds(1.5f);
+        animator.enabled = false;
+        waiting = false;
     }
 }
