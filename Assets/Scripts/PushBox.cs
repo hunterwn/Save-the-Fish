e9c5346b0dc43fb5909a0b87d30d58ca3c28ba96
jrymkos <Jrsmash@yahoo.com>
2022-04-22 17:20:41 -0400

    sound for push box

--- Assets/Scripts/PushBox.cs
+++ Assets/Scripts/PushBox.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PushBox : ActiveObject
 {
     public float pushForce = 250;
+    public AudioSource pushButtonSound;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.CompareTag("RollerBall")) {
+            pushButtonSound.Play();
             Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
             rb.AddForce(transform.up * pushForce);
         }
