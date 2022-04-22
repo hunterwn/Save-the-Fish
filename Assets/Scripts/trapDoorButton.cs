ae9b846b16cb57dc788102773b90c74b628824fe
Tri-Wing <hunterwnorman@gmail.com>
2022-04-22 17:49:37 -0400

    added door hinge sound + fixed level 3

--- Assets/Scripts/trapDoorButton.cs
+++ Assets/Scripts/trapDoorButton.cs
@@ -9,6 +9,8 @@ public class trapDoorButton : MonoBehaviour
     public Sprite pressedSprite;
     public SpriteRenderer spriteRenderer;
     public AudioSource trapDoorSound;
+    public AudioSource trapDoorSound2;
+    private bool pressed = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +23,12 @@ public class trapDoorButton : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if(!other.gameObject.CompareTag("Particle")) {
+        if(!pressed && !other.gameObject.CompareTag("Particle")) {
+            pressed = true;
             spriteRenderer.sprite = pressedSprite;
             trapDoorSound.Play();
             foreach (GameObject trapDoor in trapDoors) {
+                trapDoorSound2.Play();
                 trapDoorRB = trapDoor.GetComponent<Rigidbody2D>();
                 trapDoorRB.freezeRotation = false;
                 trapDoorRB.constraints = 0;
